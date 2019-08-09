8c0f45b46ef862ccf82399031f0060ec8587a051
naisimemeda <2514430140@qq.com>
2019-08-09 13:59:54 +0800

    删除路由

--- routes/api.php
+++ routes/api.php
@@ -30,11 +30,4 @@ Route::middleware('cors')->group(function () {
     });
 
     Route::post('orders', 'OrdersController@store')->name('orders.store');
-    Route::get('alipay', function() {
-        return app('alipay')->web([
-            'out_trade_no' => time(),
-            'total_amount' => '1',
-            'subject' => 'test subject - 测试',
-        ]);
-    });
 });
